379d6f61338ab5541378537bf97cce8d60658c8c
yohanleb <yohan.lebricquer@gmail.com>
2019-06-06 13:02:21 +0200

    Added crash reports and updated README

--- app/src/main/java/com/yohan/healthycheck/view/SplashActivity.java
+++ app/src/main/java/com/yohan/healthycheck/view/SplashActivity.java
@@ -3,11 +3,14 @@ package com.yohan.healthycheck.view;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         startActivity(new Intent(SplashActivity.this, MainActivity.class));
         finish();
     }
